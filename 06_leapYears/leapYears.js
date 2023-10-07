b72b7f10cd0096f425a8881104baaf849af5955d
Ross Sanderson <ross.h.sanderson@gmail.com>
2023-10-07 18:51:11 +1100

    leapYears are done fairly easy

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function (checkYear) {
+  if (checkYear % 4 === 0) {
+    if (checkYear % 100 === 0) {
+      if (checkYear % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      console.log(checkYear);
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+console.log(leapYears(1900));
