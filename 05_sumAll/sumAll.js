a1e06050270855860ac10ad09c2345e7d987c7a3
Ross Sanderson <ross.h.sanderson@gmail.com>
2023-10-07 18:36:21 +1100

    completed 05_sumAll && loved it!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,39 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, lastNumber) {
+  if (
+    firstNumber <= 0 ||
+    lastNumber <= 0 ||
+    typeof firstNumber != "number" ||
+    typeof lastNumber != "number"
+  ) {
+    return "ERROR";
+  } else {
+    let totalSum = 0;
+    if (firstNumber < lastNumber) {
+      for (
+        let iterationNumber = 0;
+        iterationNumber <= lastNumber - firstNumber;
+        iterationNumber++
+      ) {
+        totalSum = totalSum + firstNumber + iterationNumber;
+        console.log(totalSum);
+      }
+      console.log(totalSum);
+    } else {
+      for (
+        let iterationNumber = firstNumber;
+        iterationNumber >= lastNumber;
+        iterationNumber--
+      ) {
+        totalSum = totalSum + iterationNumber;
+        console.log(totalSum);
+      }
+      console.log(totalSum);
+    }
+    return totalSum;
+  }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(4, 1));
